14a9b312532ba08d05b6b7a490436dfccb498602
Hadrien Brochet <hadrien.brochet@inria.fr>
2024-03-21 09:35:53 +0100

    Changes to be committed: 	modified:   docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,14 @@ using Documenter, MultivariateCreativeTelescoping
 DocMeta.setdocmeta!(MultivariateCreativeTelescoping, :DocTestSetup, :(using MultivariateCreativeTelescoping); recursive=true)
 
 makedocs(sitename="MultivariateCreativeTelescoping.jl",
+        modules=[MultivariateCreativeTelescoping],
         pages = [
             "Presentation" => "index.md",
             "Quick start" => "QuickStart.md"
         ])
 
 deploydocs(
-    repo = "github.com/HBrochet/MultivariateCreativeTelescoping.jl.git",
+    repo = "github.com/HBrochet/MultivariateCreativeTelescoping.jl",
+    target = "build",
+    branch = "gh-pages",
 )
