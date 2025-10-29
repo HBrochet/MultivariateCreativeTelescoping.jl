92ca889f61df13c843b63a038ab3f71c7ccef8aa
Hadrien Brochet <hadrien.brochet@hotmail.fr>
2025-10-29 13:44:53 +0100

    factoring code in TypeCoefficient and writing tests

--- src/TypeCoefficient.jl
+++ src/TypeCoefficient.jl
@@ -1,22 +1,28 @@
-# partially adapted from a file of Pierre Lairez
+# partially adapted from a file by Pierre Lairez
+
+# File defining the coefficents types that can be used in this package.  
+# The coefficients are stored in a type T, except for the output of addmul and submul operations 
+# which are stored in a type Tbuf. A context is defined to store necessary informations in order to 
+# perform operations on coefficients.  
+
+
+### Abstract types 
 
 abstract type AbsCoefficient end
 
-#= Expect:
-    - Base.isone(::T) :: Bool
-    - Base.iszero(::T) :: Bool
-    - add(::NmodLikeΓ{T}, ::T, ::T) :: T
-    - mul(::NmodLikeΓ{T}, ::T, ::T) :: T
-    - inv(::NmodLikeΓ{T}, ::T) :: T
-    - submul(::NmodLikeΓ{T, Tbuf}, ::Tbuf, ::T, ::T) :: Tbuf
-    - normal(::NmodLikeΓ{T, Tbuf}, ::Tbuf) :: Tbuf
-    - inflate(::NmodLikeΓ{T, Tbuf}, ::T) :: Tbuf
-    - deflate(::NmodLikeΓ{T, Tbuf}, ::Tbuf) :: T
-
-    - convert(::NomodLikeΓ{T, Tbuf}, ::Integer) :: T
-    - convertn
-    - opp 
-    - normal =#
+#  Expect:
+#     - Base.isone(::T) :: Bool
+#     - Base.iszero(::T) :: Bool
+#     - add(::T, ::T, ::NmodLikeΓ{T}) :: T
+#     - mul(::T, ::T, ::NmodLikeΓ{T}) :: T
+#     - inv(::T, ::NmodLikeΓ{T}) :: T
+#     - opp(::T, ::NmodLikeΓ{T}) :: T
+#     - submul(::Tbuf, ::T, ::T, ::NmodLikeΓ{T, Tbuf}) :: Tbuf
+#     - normal(::Tbuf, ::NmodLikeΓ{T, Tbuf}) :: Tbuf
+#     - Base.convert(::Integer, ::NomodLikeΓ{T, Tbuf}) :: T
+#     - convertn(::Integer, ::NomodLikeΓ{T, Tbuf}) :: T
+#     - inflate(::T, ::NmodLikeΓ{T, Tbuf}) :: Tbuf
+#     - deflate(::Tbuf, ::NmodLikeΓ{T, Tbuf}) :: T
 
 
 abstract type AbsContextCoeff{T, Tbuf} end
@@ -36,9 +42,6 @@ deflate(x :: Tbuf,::NmodLikeΓ{T, Tbuf}) where {T, Tbuf} = x%T
 
 abstract type NmodΓ{T<:Unsigned, Tbuf<:Unsigned}<: NmodLikeΓ{T,Tbuf} end
 
-# (ctx::NmodΓ)(a::AA.ResElem) = ctx(a.data)
-# abstractalgebra(ctx :: NmodΓ) = Nemo.NmodRing(UInt64(ctx.char))
-
 Base.zero(ctx :: NmodΓ{T,Tbuf}) where {T, Tbuf} = T(0) 
 Base.one(ctx :: NmodΓ{T,Tbuf}) where {T, Tbuf} = T(1) 
 
@@ -48,7 +51,7 @@ Base.one(T :: DataType,ctx :: NmodΓ) = T(1)
 Base.iszero(x :: T, ctx :: NmodΓ{TT, Tbuf}) where {T,TT, Tbuf} = x == T(0)
 Base.isone(x :: T, ctx :: NmodΓ{T, Tbuf}) where {T, Tbuf} = x == T(1)
 
-#.. 32-bit modulus and 64-bit buffer
+#.. 32-bit modulus and 64-bit buffer 
 
 struct Nmod32Γ<:NmodΓ{UInt32, UInt64}
     char::UInt32
@@ -63,7 +66,7 @@ struct Nmod32Γ<:NmodΓ{UInt32, UInt64}
     end
 end
 
-convert(T :: Integer,ctx :: Nmod32Γ) = UInt32(T)
+Base.convert(T :: Integer,ctx :: Nmod32Γ) = UInt32(T)
 #... arithmetic operations
 # Must implement inv, mul, addmul and normal
 
@@ -76,6 +79,7 @@ convert(T :: Integer,ctx :: Nmod32Γ) = UInt32(T)
     end
 end
 
+# assume that we do not take the opposite of 0
 function opp(a::UInt32, ctx::Nmod32Γ)
     return ctx.char - a
 end
@@ -102,7 +106,7 @@ end
 
 
 
-function inv(a::UInt32, ctx::Nmod32Γ)
+function Base.inv(a::UInt32, ctx::Nmod32Γ)
     invmod(UInt64(a), UInt64(ctx.char)) % UInt32
 end
 
@@ -119,64 +123,58 @@ function convertn(x :: Integer,ctx :: Nmod32Γ)
 end
 
 
-#.. 32-bit modulus with 128-bits buffer
-# currently not used
-
-mutable struct Nmod32xΓ<:NmodΓ{UInt32, UInt128}
-    char::UInt32
-    twotothe64::UInt32        # 2^64 mod char
-
-    function Nmod32xΓ(char)
-        uchar = UInt32(char)
-        p = (UInt64(2^32) % uchar)^2 % uchar % UInt32
-
-        new(char, p)
-    end
-end
-
-
-#... arithmetic operations
-# Must implement inv, mul, submul and normal
-
-function add(ctx::Nmod32xΓ, a::UInt32, b::UInt32)
-    c0 = a+b
-    c1 = c0 - ctx.char
-    d = max(c0, c1)
-    return d
-end
-
-@inline function addmul(ctx::Nmod32xΓ, a::UInt128, b::UInt32, c::UInt32)
-    z0 = a + UInt128(UInt64(b)*UInt64(c))
-    return z0
-end
-
-
-@inline function submul(ctx::Nmod32xΓ, a::UInt128, b::UInt32, c::UInt32)
-    z0 = a - UInt128(UInt64(b)*UInt64(c))
-    return z0
-end
-
-function inv(ctx::Nmod32xΓ, a::UInt32)
-    UInt32(invmod(UInt64(a), UInt64(ctx.char)))
-end
-
-function mul(ctx::Nmod32xΓ, a::UInt32, b::UInt32)
-    UInt32((UInt64(a)*UInt64(b) % ctx.char) % UInt32)
-end
-
-function normal(ctx::Nmod32xΓ, a::UInt128)
-    return a % ctx.char
-    # hi = (a >> 64) % UInt64
-    # lo = a % UInt64
-    # add(ctx,
-    #     ctx.twotothe64 * hi % ctx.char % UInt32,
-    #     lo % ctx.char % UInt32)
-end
-
+### Rational functions
 
 #. Univariate rational functions
 abstract type RatFunCtx{T,TT} <: NmodLikeΓ{T, TT} end
 abstract type UnivRatFunCtx{T,TT} <: RatFunCtx{T,TT} end
+
+const RatFun = Union{
+    Generic.FracFieldElem{fpPolyRingElem},
+    Generic.FracFieldElem{FpPolyRingElem},
+    Generic.FracFieldElem{ZZPolyRingElem},
+    Generic.FracFieldElem{fpMPolyRingElem},
+    Generic.FracFieldElem{FpMPolyRingElem},
+    Generic.FracFieldElem{ZZMPolyRingElem},
+}
+
+
+# shared for all rational-function contexts
+opp(a :: RatFun, ::RatFunCtx) = -a
+add(a :: RatFun, b :: RatFun, ::RatFunCtx) = a + b
+sub(a :: RatFun, b :: RatFun, ::RatFunCtx) = a - b
+mul(a :: RatFun, b :: RatFun, ::RatFunCtx) = a * b
+Base.inv(a :: RatFun, ::RatFunCtx) = inv(a)
+submul(a :: RatFun, b :: RatFun, c :: RatFun, ::RatFunCtx) = a - b * c
+normal(a :: RatFun, ::RatFunCtx) = a
+inflate(a :: RatFun, ::RatFunCtx) = a
+deflate(a :: RatFun, ::RatFunCtx) = a
+Base.convert(a, ctx::RatFunCtx) = ctx.F(a)
+convertn(a, ctx::RatFunCtx) = ctx.F(a)
+
+Base.one(ctx::RatFunCtx) = ctx.F(1)
+Base.zero(::Type{T}, ctx::RatFunCtx) where {T} = ctx.F(0)
+Base.zero(ctx::RatFunCtx) = ctx.F(0)
+Base.iszero(x :: RatFun, ctx::RatFunCtx) = x == ctx.F(0)
+Base.isone(x :: RatFun, ctx::RatFunCtx) = x == ctx.F(1)
+
+function evaluate(a::RatFun, p::Vector{S}) where {S}
+    num = Nemo.numerator(a, false)
+    den = Nemo.denominator(a, false)
+
+    if a isa UnivRatFunModp || a isa UnivRatFunModP || a isa UnivRatFunQQ
+        length(p) == 1 || error("the vector p has size $(length(p))")
+        return Nemo.evaluate(a, p[1])
+    end
+
+    return Nemo.evaluate(num, p) // Nemo.evaluate(den, p)
+end
+
+function evaluate(a::RatFun, p)
+    num = Nemo.numerator(a, false)
+    den = Nemo.denominator(a, false)
+    return Nemo.evaluate(num, p) // Nemo.evaluate(den, p)
+end
 
 #. ratfun mod small p 
 
@@ -189,35 +187,6 @@ struct UnivRatFunModpCtx <: UnivRatFunCtx{UnivRatFunModp, UnivRatFunModp}
     char :: Int  
 end
 
-opp(a :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = - a
-add(a :: UnivRatFunModp, b :: UnivRatFunModp, ctx ::UnivRatFunModpCtx) = a + b 
-sub(a :: UnivRatFunModp, b :: UnivRatFunModp, ctx ::UnivRatFunModpCtx) = a - b 
-mul(a :: UnivRatFunModp, b :: UnivRatFunModp, ctx ::UnivRatFunModpCtx) = a * b 
-inv(a :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = 1/a 
-submul(a :: UnivRatFunModp, b :: UnivRatFunModp, c :: UnivRatFunModp, ctx ::UnivRatFunModpCtx) = a - b * c 
-normal(a :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = a 
-inflate(a :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = a 
-deflate(a :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = a 
-convert(a :: Integer, ctx :: UnivRatFunModpCtx) = ctx.F(a)
-convertn(a :: Integer, ctx :: UnivRatFunModpCtx) = ctx.F(a)
-
-Base.one(ctx :: UnivRatFunModpCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: UnivRatFunModpCtx) where T = ctx.F(0)
-Base.zero(ctx :: UnivRatFunModpCtx) = ctx.F(0)
-
-Base.iszero(x :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = x == ctx.F(0)
-Base.isone(x :: UnivRatFunModp, ctx :: UnivRatFunModpCtx) = x == ctx.F(1)
-
-function evaluate(a :: UnivRatFunModp, p :: T) where T 
-    return Nemo.evaluate(Nemo.numerator(a,false),p)//Nemo.evaluate(Nemo.denominator(a,false),p)
-end
-
-function evaluate(a :: UnivRatFunModp, p :: Vector{T}) where T 
-    if length(p) > 1 
-        error("the vector p has size > 1")
-    end
-    return Nemo.evaluate(Nemo.numerator(a,false),p[1])//Nemo.evaluate(Nemo.denominator(a,false),p[1])
-end
 
 # ratfun mod large p 
 
@@ -227,29 +196,10 @@ struct UnivRatFunModPCtx <: UnivRatFunCtx{UnivRatFunModP, UnivRatFunModP}
     F :: Generic.FracField{Nemo.FpPolyRingElem}
     R :: FpPolyRing
     vars :: Vector{FpPolyRingElem}
-    char :: Int  
+    char :: ZZRingElem 
 end
 
-opp(a :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = - a
-add(a :: UnivRatFunModP, b :: UnivRatFunModP, ctx ::UnivRatFunModPCtx) = a + b 
-sub(a :: UnivRatFunModP, b :: UnivRatFunModP, ctx ::UnivRatFunModPCtx) = a - b 
-mul(a :: UnivRatFunModP, b :: UnivRatFunModP, ctx ::UnivRatFunModPCtx) = a * b 
-inv(a :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = 1/a 
-submul(a :: UnivRatFunModP, b :: UnivRatFunModP, c :: UnivRatFunModP, ctx ::UnivRatFunModPCtx) = a - b * c 
-normal(a :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = a 
-inflate(a :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = a 
-deflate(a :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = a 
-convert(a :: Integer, ctx :: UnivRatFunModPCtx) = ctx.F(a)
-convertn(a :: Integer, ctx :: UnivRatFunModPCtx) = ctx.F(a)
-
-Base.one(ctx :: UnivRatFunModPCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: UnivRatFunModPCtx) where T = ctx.F(0)
-Base.zero(ctx :: UnivRatFunModPCtx) = ctx.F(0)
-
-Base.iszero(x :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = x == ctx.F(0)
-Base.isone(x :: UnivRatFunModP, ctx :: UnivRatFunModPCtx) = x == ctx.F(1)
-
-##
+# rafun over Q
 const UnivRatFunQQ = Generic.FracFieldElem{ZZPolyRingElem}
 
 struct UnivRatFunQQCtx <: UnivRatFunCtx{UnivRatFunQQ,UnivRatFunQQ}
@@ -260,28 +210,8 @@ struct UnivRatFunQQCtx <: UnivRatFunCtx{UnivRatFunQQ,UnivRatFunQQ}
     UnivRatFunQQCtx(F,R,v) = new(F,R,v,0)
 end
 
-opp(a :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = - a
-add(a :: UnivRatFunQQ, b :: UnivRatFunQQ, ctx ::UnivRatFunQQCtx) = a + b 
-sub(a :: UnivRatFunQQ, b :: UnivRatFunQQ, ctx ::UnivRatFunQQCtx) = a - b 
-mul(a :: UnivRatFunQQ, b :: UnivRatFunQQ, ctx ::UnivRatFunQQCtx) = a * b 
-inv(a :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = 1/a 
-submul(a :: UnivRatFunQQ, b :: UnivRatFunQQ, c :: UnivRatFunQQ, ctx ::UnivRatFunQQCtx) = a - b * c 
-normal(a :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = a 
-inflate(a :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = a 
-deflate(a :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = a 
-convert(a :: Int, ctx :: UnivRatFunQQCtx) = ctx.F(a)
-convertn(a :: Integer, ctx :: UnivRatFunQQCtx) = ctx.F(a)
-
-Base.one(ctx :: UnivRatFunQQCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: UnivRatFunQQCtx) where T = ctx.F(0)
-Base.zero(ctx :: UnivRatFunQQCtx) = ctx.F(0)
-
-Base.iszero(x :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = x == ctx.F(0)
-Base.isone(x :: UnivRatFunQQ, ctx :: UnivRatFunQQCtx) = x == ctx.F(1)
-
-
-
 #. Multivariate rational functions
+
 abstract type MRatFunCtx{T,TT} <: RatFunCtx{T,TT}  end
 
 #. ratfun mod small p 
@@ -295,28 +225,6 @@ struct RatFunModpCtx <: MRatFunCtx{RatFunModp, RatFunModp}
     char :: Int  
 end
 
-opp(a :: RatFunModp, ctx :: RatFunModpCtx) = - a
-add(a :: RatFunModp, b :: RatFunModp, ctx ::RatFunModpCtx) = a + b 
-sub(a :: RatFunModp, b :: RatFunModp, ctx ::RatFunModpCtx) = a - b 
-mul(a :: RatFunModp, b :: RatFunModp, ctx ::RatFunModpCtx) = a * b 
-inv(a :: RatFunModp, ctx :: RatFunModpCtx) = 1/a 
-submul(a :: RatFunModp, b :: RatFunModp, c :: RatFunModp, ctx ::RatFunModpCtx) = a - b * c 
-normal(a :: RatFunModp, ctx :: RatFunModpCtx) = a 
-inflate(a :: RatFunModp, ctx :: RatFunModpCtx) = a 
-deflate(a :: RatFunModp, ctx :: RatFunModpCtx) = a 
-convert(a :: Integer, ctx :: RatFunModpCtx) = ctx.F(a)
-convertn(a :: Integer, ctx :: RatFunModpCtx) = ctx.F(a)
-
-Base.one(ctx :: RatFunModpCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: RatFunModpCtx) where T = ctx.F(0)
-Base.zero(ctx :: RatFunModpCtx) = ctx.F(0)
-
-Base.iszero(x :: RatFunModp, ctx :: RatFunModpCtx) = x == ctx.F(0)
-Base.isone(x :: RatFunModp, ctx :: RatFunModpCtx) = x == ctx.F(1)
-
-function evaluate(a :: RatFunModp, p :: Vector{T}) where T
-    return Nemo.evaluate(Nemo.numerator(a,false),p)//Nemo.evaluate(Nemo.denominator(a,false),p)
-end
 
 # ratfun mod large p 
 
@@ -326,27 +234,8 @@ struct RatFunModPCtx <: MRatFunCtx{RatFunModP, RatFunModP}
     F :: Generic.FracField{Nemo.FpMPolyRingElem}
     R :: FpMPolyRing
     vars :: Vector{FpMPolyRingElem}
-    char :: Int  
+    char :: ZZRingElem  
 end
-
-opp(a :: RatFunModP, ctx :: RatFunModPCtx) = - a
-add(a :: RatFunModP, b :: RatFunModP, ctx ::RatFunModPCtx) = a + b 
-sub(a :: RatFunModP, b :: RatFunModP, ctx ::RatFunModPCtx) = a - b 
-mul(a :: RatFunModP, b :: RatFunModP, ctx ::RatFunModPCtx) = a * b 
-inv(a :: RatFunModP, ctx :: RatFunModPCtx) = 1/a 
-submul(a :: RatFunModP, b :: RatFunModP, c :: RatFunModP, ctx ::RatFunModPCtx) = a - b * c 
-normal(a :: RatFunModP, ctx :: RatFunModPCtx) = a 
-inflate(a :: RatFunModP, ctx :: RatFunModPCtx) = a 
-deflate(a :: RatFunModP, ctx :: RatFunModPCtx) = a 
-convert(a :: Integer, ctx :: RatFunModPCtx) = ctx.F(a)
-convertn(a :: Integer, ctx :: RatFunModPCtx) = ctx.F(a)
-
-Base.one(ctx :: RatFunModPCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: RatFunModPCtx) where T = ctx.F(0)
-Base.zero(ctx :: RatFunModPCtx) = ctx.F(0)
-
-Base.iszero(x :: RatFunModP, ctx :: RatFunModPCtx) = x == ctx.F(0)
-Base.isone(x :: RatFunModP, ctx :: RatFunModPCtx) = x == ctx.F(1)
 
 ##
 const RatFunQQ = Generic.FracFieldElem{ZZMPolyRingElem}
@@ -359,76 +248,27 @@ struct RatFunQQCtx <: MRatFunCtx{RatFunQQ,RatFunQQ}
     RatFunQQCtx(F,R,v) = new(F,R,v,0)
 end
 
-opp(a :: RatFunQQ, ctx :: RatFunQQCtx) = - a
-add(a :: RatFunQQ, b :: RatFunQQ, ctx ::RatFunQQCtx) = a + b 
-sub(a :: RatFunQQ, b :: RatFunQQ, ctx ::RatFunQQCtx) = a - b 
-mul(a :: RatFunQQ, b :: RatFunQQ, ctx ::RatFunQQCtx) = a * b 
-inv(a :: RatFunQQ, ctx :: RatFunQQCtx) = 1/a 
-submul(a :: RatFunQQ, b :: RatFunQQ, c :: RatFunQQ, ctx ::RatFunQQCtx) = a - b * c 
-normal(a :: RatFunQQ, ctx :: RatFunQQCtx) = a 
-inflate(a :: RatFunQQ, ctx :: RatFunQQCtx) = a 
-deflate(a :: RatFunQQ, ctx :: RatFunQQCtx) = a 
-convert(a, ctx :: RatFunQQCtx) = ctx.F(a)
-convertn(a, ctx :: RatFunQQCtx) = ctx.F(a)
-
-Base.one(ctx :: RatFunQQCtx) = ctx.F(1)
-Base.zero(x :: T, ctx :: RatFunQQCtx) where T = ctx.F(0)
-Base.zero(ctx :: RatFunQQCtx) = ctx.F(0)
-
-Base.iszero(x :: RatFunQQ, ctx :: RatFunQQCtx) = x == ctx.F(0)
-Base.isone(x :: RatFunQQ, ctx :: RatFunQQCtx) = x == ctx.F(1)
-
-function evaluate(a :: RatFunQQ, p :: Vector{T}) where T
-    return Nemo.evaluate(Nemo.numerator(a,false),p)//Nemo.evaluate(Nemo.denominator(a,false),p)
-end
-
 ## defining rational coefficient 
 struct QQCtx <: NmodLikeΓ{QQFieldElem,QQFieldElem} 
     char :: Int 
     QQCtx() = new(0)
 end
 
-opp(a :: QQFieldElem, ctx :: QQCtx) = - a
-add(a :: QQFieldElem, b :: QQFieldElem, ctx ::QQCtx) = a + b 
-sub(a :: QQFieldElem, b :: QQFieldElem, ctx ::QQCtx) = a - b 
-mul(a :: QQFieldElem, b :: QQFieldElem, ctx ::QQCtx) = a * b 
-inv(a :: QQFieldElem, ctx :: QQCtx) = 1/a 
-submul(a :: QQFieldElem, b :: QQFieldElem, c :: QQFieldElem, ctx ::QQCtx) = a - b * c 
-normal(a :: QQFieldElem, ctx :: QQCtx) = a 
-inflate(a :: QQFieldElem, ctx :: QQCtx) = a 
-deflate(a :: QQFieldElem, ctx :: QQCtx) = a 
-convert(a :: Int, ctx :: QQCtx) = QQ(a)
-convertn(a :: Integer, ctx :: QQCtx) = QQ(a)
+opp(a :: QQFieldElem, :: QQCtx) = - a
+add(a :: QQFieldElem, b :: QQFieldElem, ::QQCtx) = a + b 
+sub(a :: QQFieldElem, b :: QQFieldElem, ::QQCtx) = a - b 
+mul(a :: QQFieldElem, b :: QQFieldElem, ::QQCtx) = a * b 
+Base.inv(a :: QQFieldElem, :: QQCtx) = 1/a 
+submul(a :: QQFieldElem, b :: QQFieldElem, c :: QQFieldElem, ::QQCtx) = a - b * c 
+normal(a :: QQFieldElem, :: QQCtx) = a 
+inflate(a :: QQFieldElem, :: QQCtx) = a 
+deflate(a :: QQFieldElem, :: QQCtx) = a 
+Base.convert(a :: Int, :: QQCtx) = QQ(a)
+convertn(a :: Integer, :: QQCtx) = QQ(a)
 
-Base.one(ctx :: QQCtx) = QQ(1)
-Base.zero(x :: T, ctx :: QQCtx) where T = QQ(0)
-Base.zero(ctx :: QQCtx) = QQ(0)
+Base.one(:: QQCtx) = QQ(1)
+Base.zero(:: T,:: QQCtx) where T = QQ(0)
+Base.zero(:: QQCtx) = QQ(0)
 
-Base.iszero(x :: QQFieldElem, ctx :: QQCtx) = x == QQ(0)
-Base.isone(x :: QQFieldElem, ctx :: QQCtx) = x == QQ(1)
-
-
-# struct SLPCtx{N,T,P} <: AbsContextCoeff{SLP{N,T}, SLP{N,T}}
-#     R :: P
-# end
-
-# SLPCtx(N :: Int,R :: ZZMPolyRing) = SLPCtx{N,elem_type(R),typeof(R)}(R)
-
-
-# add(a :: SLP, b :: SLP,:: SLPCtx) = SLP(:add,a,b)
-# sub(a :: SLP, b :: SLP,:: SLPCtx) = SLP(:sub,a,b)
-# mul(a :: SLP, b :: SLP,:: SLPCtx) = SLP(:mul,a,b)
-# opp(a :: SLP,:: SLPCtx) = SLP(:opp,a)
-# shift(a :: SLP, m::SVector,:: SLPCtx) = SLP(m,:shift,nothing,a,nothing)
-# # mulshift(a :: SLP, b :: SLP,m::SVector,:: SLPCtx) = SLP(m,:mulshift,nothing,a,b)
-
-# # creates a leaf
-# SLP(a :: K, ctx :: SLPCtx{N,T,P}) where {K,N,T,P} =  SLP{N,T}(SVector{N,Int16}(0 for i in 1:N),:pol,ctx.R(a),nothing,nothing)
-
-# convertn(a :: T,ctx::SLPCtx) where T = SLP(ctx.R(a),ctx)
-# Base.one(ctx :: SLPCtx) = convertn(1,ctx)
-# Base.zero(ctx :: SLPCtx) = convertn(0,ctx)
-
-            
-
-
+Base.iszero(x :: QQFieldElem, :: QQCtx) = x == QQ(0)
+Base.isone(x :: QQFieldElem, :: QQCtx) = x == QQ(1)
---
+++ test/typecoefficient.jl
@@ -0,0 +1,195 @@
+using Test
+using Nemo: finite_field, polynomial_ring, fraction_field, numerator, denominator, ZZ, QQ
+using Nemo.Native: GF
+using MultivariateCreativeTelescoping
+
+@testset "TypeCoefficient: Nmod32Γ arithmetic" begin
+    ctx = Nmod32Γ(17)
+    a = UInt32(3)
+    b = UInt32(5)
+
+    @test @inferred(add(a, b, ctx)) == UInt32(8)
+    @test @inferred(sub(a, b, ctx)) == UInt32(15)
+    @test @inferred(opp(b, ctx)) == UInt32(12)
+    @test @inferred(mul(a, b, ctx)) == UInt32(15)
+    @test @inferred(inv(b, ctx)) == UInt32(7)
+
+    prod_buf = @inferred(addmul(UInt64(14), a, b, ctx))
+    @test @inferred normal(prod_buf, ctx) == 12
+    sub_buf = @inferred(submul(prod_buf, a, b, ctx))
+    @test @inferred normal(sub_buf, ctx) == UInt32(14)
+
+    inflated = @inferred(inflate(a, ctx))
+    @test inflated == UInt64(a)
+    @test @inferred(deflate(inflated, ctx)) == a
+    @test @inferred(normal(UInt64(34), ctx)) == UInt32(0)
+    @test @inferred(convertn(-3, ctx)) == UInt32(14)
+end
+
+@testset "TypeCoefficient: rational univariate mod small p" begin
+    p = 7
+    Fp = GF(p)
+    R, x = polynomial_ring(Fp, "x")
+    F = fraction_field(R)
+    ctx = UnivRatFunModpCtx(F, R, [x], p)
+
+    f = F(x + 1)
+    g = F(x^2 + 1)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    inv_f = @inferred(inv(f, ctx))
+    @test mul(inv_f,f,ctx) == one(F)
+    @test @inferred(normal(f, ctx)) === f
+    @test @inferred(inflate(f, ctx)) === f
+    @test @inferred(deflate(f, ctx)) === f
+
+    @test @inferred(convert(2, ctx)) == ctx.F(2)
+    @test @inferred(convertn(-3, ctx)) == ctx.F(-3)
+
+    val = Fp(3)
+
+    @test @inferred(evaluate(f, val)) == Fp(4)
+    @test @inferred(evaluate(f, [val])) == Fp(4)
+    @test_throws ErrorException evaluate(f, [val, val])
+end
+
+@testset "TypeCoefficient: rational univariate mod large p" begin
+    p = ZZ(18446744073709551629)
+    Fp = GF(p)
+    R, x = polynomial_ring(Fp, "x")
+    F = fraction_field(R)
+    ctx = UnivRatFunModPCtx(F, R, [x], p)
+
+    f = F(x + 1)
+    g = F(x^2 + 1)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    inv_f = @inferred(inv(f, ctx))
+    @test @inferred mul(inv_f,f,ctx) == one(F)
+    @test @inferred(normal(f, ctx)) === f
+    @test @inferred(inflate(f, ctx)) === f
+    @test @inferred(deflate(f, ctx)) === f
+
+    @test @inferred(convert(2, ctx)) == ctx.F(2)
+    @test @inferred(convertn(-3, ctx)) == ctx.F(-3)
+
+    val = Fp(3)
+    @test @inferred(evaluate(f, val)) == F(4)
+    @test @inferred(evaluate(f, [val])) == F(4)
+    @test_throws ErrorException evaluate(f, [val, val])
+end
+
+@testset "TypeCoefficient: rational univariate QQ context" begin
+    R, x = polynomial_ring(ZZ, "x")
+    F = fraction_field(R)
+    ctx = UnivRatFunQQCtx(F, R, [x])
+
+    f = F(x + 1)
+    g = F(x^2 + 2)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    inv_f = @inferred(inv(f, ctx))
+    @test @inferred(mul(inv_f,f,ctx)) == one(F)
+    @test @inferred(normal(f, ctx)) === f
+    @test @inferred(inflate(f, ctx)) === f
+    @test @inferred(deflate(f, ctx)) === f
+
+    @test @inferred(convert(5, ctx)) == ctx.F(5)
+    @test @inferred(convertn(-4, ctx)) == ctx.F(-4)
+
+    val = ZZ(3)
+    @test @inferred(evaluate(f, val)) == ZZ(4)
+    @test @inferred(evaluate(f, [val])) == ZZ(4)
+    @test_throws ErrorException evaluate(f, [val, val])
+end
+
+@testset "TypeCoefficient: rational multivariate mod small p" begin
+    p = 11
+    Fp = GF(p)
+    R, (x, y) = polynomial_ring(Fp, ["x", "y"])
+    F = fraction_field(R)
+    ctx = RatFunModpCtx(F, R, [x, y], p)
+
+    f = F(x + y)
+    g = F(x * y + 1)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    @test @inferred(mul(f, g, ctx)) == f * g
+    @test @inferred(submul(g, f, g, ctx)) == g - f * g
+    @test @inferred(opp(f, ctx)) == -f
+    inv_f = @inferred(inv(f, ctx))
+    @test @inferred(mul(inv_f, f, ctx)) == one(F)
+    @test @inferred(normal(f, ctx)) === f
+    @test @inferred(inflate(f, ctx)) === f
+    @test @inferred(deflate(f, ctx)) === f
+
+    @test @inferred(convert(5, ctx)) == ctx.F(5)
+    @test @inferred(convertn(-9, ctx)) == ctx.F(-9)
+
+    vals = [Fp(2), Fp(3)]
+    @test @inferred(evaluate(f, vals)) == ctx.F(5)
+end
+
+@testset "TypeCoefficient: rational multivariate mod large p" begin
+    p = ZZ(18446744073709551629)
+    Fp = GF(p)
+    R, (x, y) = polynomial_ring(Fp, ["x", "y"])
+    F = fraction_field(R)
+    ctx = RatFunModPCtx(F, R, [x, y], p)
+
+    f = F(x + y + 1)
+    g = F(x * y + 1)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    @test @inferred(mul(f, g, ctx)) == f * g
+    @test @inferred(submul(g, f, g, ctx)) == g - f * g
+    @test @inferred(opp(f, ctx)) == -f
+
+    @test @inferred(convert(7, ctx)) == ctx.F(7)
+    @test @inferred(convertn(-9, ctx)) == ctx.F(-9)
+
+    vals = [Fp(2), Fp(4)]
+    @test @inferred(evaluate(f, vals)) == ctx.F(7)
+end
+
+@testset "TypeCoefficient: rational multivariate QQ context" begin
+    R, (x, y) = polynomial_ring(ZZ, ["x", "y"])
+    F = fraction_field(R)
+    ctx = RatFunQQCtx(F, R, [x, y])
+
+    f = F(x + y)
+    g = F(x * y + 1)
+
+    @test @inferred(add(f, g, ctx)) == f + g
+    @test @inferred(sub(g, f, ctx)) == g - f
+    @test @inferred(mul(f, g, ctx)) == f * g
+    @test @inferred(submul(g, f, g, ctx)) == g - f * g
+    @test @inferred(opp(f, ctx)) == -f
+
+    @test @inferred(convert(5, ctx)) == ctx.F(5)
+    @test @inferred(convertn(-9, ctx)) == ctx.F(-9)
+
+    vals = [ZZ(2), ZZ(3)]
+    @test @inferred(evaluate(f, vals)) == QQ(5)
+end
+
+@testset "TypeCoefficient: QQCtx arithmetic" begin
+    ctx = QQCtx()
+    a = QQ(2)
+    b = QQ(3)
+
+    @test @inferred(add(a, b, ctx)) == QQ(5)
+    @test @inferred(sub(b, a, ctx)) == QQ(1)
+    @test @inferred(mul(a, b, ctx)) == QQ(6)
+    inv_a = @inferred(inv(a, ctx))
+    @test inv_a * a == QQ(1)
+    @test @inferred(submul(b, a, b, ctx)) == QQ(-3)
+    @test @inferred(convert(-4, ctx)) == QQ(-4)
+    @test @inferred(convertn(7, ctx)) == QQ(7)
+    @test @inferred(normal(a, ctx)) === a
+end
