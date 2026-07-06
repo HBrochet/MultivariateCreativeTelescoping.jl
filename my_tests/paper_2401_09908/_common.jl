4aef287be6bd0b802acdee8738e0d023819ece0b
Hadrien Brochet <138566361+HBrochet@users.noreply.github.com>
2026-07-06 17:46:54 +0200

    Delete my_tests/paper_2401_09908 directory

--- my_tests/paper_2401_09908/_common.jl
+++
@@ -1,7 +0,0 @@
-using MultivariateCreativeTelescoping
-
-function annihilator_from_dfinite_parser(expr::AbstractString, A::OreAlg)
-    Ap = to_ore_alg_with_rat_coeffs(A)
-    annp = dfinite_expr_to_ann(expr, Ap)
-    return map_algebras(annp, Ap, A)
-end
